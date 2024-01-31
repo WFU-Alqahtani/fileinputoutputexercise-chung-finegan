e82d8f8e66b257d363ca85e73dd18ebfe8158002
AlexanderChung04 <chunab23@wfu.edu>
2024-01-31 12:11:03 -0500

    Printed all rows, then all movie names

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,6 +1,38 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileInputOutput {
 
     public static void main(String[] args) {
+
+        FileInputStream myFile = null;
+        FileInputStream myFile2 = null;
+
+        try {
+            myFile = new FileInputStream("src/moviesDataSet.csv");
+            myFile2 = new FileInputStream("src/moviesDataSet.csv");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Could not open input file - ending program");
+            System.exit(1);
+        }
+
+        Scanner fileReader2 = new Scanner(myFile2);
+        while(fileReader2.hasNextLine()){
+            String data = fileReader2.nextLine();
+            System.out.println(data);
+        }
+
+        Scanner fileReader = new Scanner(myFile);
+        while(fileReader.hasNextLine()){
+            String data = fileReader.nextLine();
+            String[] arrOfStr = data.split(",");
+            System.out.println(arrOfStr[2]);
+        }
+
+        fileReader.close();
+
 //       1) Open the given file inside src called moviesDataSet.csv
 //       2) Read each row in the dataset
 //       3) Print each row to the screen
